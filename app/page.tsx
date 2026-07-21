7c65b4288a08cb3440392bf213c0ec4bd63f94d3
anonymous1495 <mrunali.db@gmail.com>
2026-07-22 00:39:50 +0100

    fixed bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useChat } from 'ai/react';
+import { useChat } from '@ai-sdk/react';
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, isLoading } = useChat();
